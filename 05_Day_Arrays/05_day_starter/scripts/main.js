4897f1eeabb354504b36c0f5fdbf11f9a332099a
Ezel Espera <zellai022890@gmail.com>
2023-11-12 07:51:44 +0800

    Day 5: Exercise Lvl.2 no.5

--- 05_Day_Arrays/05_day_starter/scripts/main.js
+++ 05_Day_Arrays/05_day_starter/scripts/main.js
@@ -32,4 +32,12 @@ if (hasCountry == true){
 }
 
 
-
+// 5. In the webTechs array check if Sass exists in the array and if it exists print 
+// 'Sass is a CSS preprocess'. If it does not exist add Sass to the array and print the array.
+const hasSass = webTechs.includes('Sass')
+if (hasSass == true){
+    console.log('Sass is a CSS preprocess')
+}else {
+    webTechs.push('Sass')
+    console.log(webTechs)
+}
