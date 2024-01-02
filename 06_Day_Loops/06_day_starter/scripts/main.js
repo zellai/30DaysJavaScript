91038d54c7e153d55e1b836d436d7a544e0f839e
Ezel Espera <zellai022890@gmail.com>
2024-01-02 16:58:00 +0800

    Day 6 Loops: Exercise Level 2 no.5

--- 06_Day_Loops/06_day_starter/scripts/main.js
+++ 06_Day_Loops/06_day_starter/scripts/main.js
@@ -213,6 +213,7 @@ console.log(arr);
 let result = '';
 const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 const charactersLength = characters.length;
+
 let counter = 0;
 while (counter < 6) {
   result += characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -222,5 +223,158 @@ console.log(result);
 
 */
 
+/* EXERCISE 2
+1. Develop a small script which generate any number of characters random id:
+  fe3jo1gl124g
+  xkqci4utda1lmbelpkm03rba */
+
+// let result = '';
+// const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+// const charactersLength = characters.length;
+
+// let counter = 0;
+// while (counter < Math.floor(Math.random() * 50)) {
+//   result += characters.charAt(Math.floor(Math.random() * charactersLength));
+//   counter ++;
+// }
+// console.log(result);
+
+/* 2. Write a script which generates a random hexadecimal number.
+'#ee33df'*/
+
+// let result = '';
+// const characters = '0123456789abcdef';
+// const charactersLength = characters.length;
+
+// let counter = 0;
+// while (counter < 6) {
+//   result += characters.charAt(Math.floor(Math.random() * charactersLength));
+//   counter ++;
+// }
+// console.log("#" +result);
+
+/*3. Write a script which generates a random rgb color number.
+rgb(240,180,80)*/
+
+// let counter = 0;
+// let arr = [];
+// while (counter < 3) {
+//   num = Math.floor(Math.random() * 255);
+//   counter ++;
+//   arr.push(num);
+// }
+// console.log("rgb(" + arr[0] +"," + arr[1] + "," + arr[2]+")");
 
 
+/*4. Using the above countries array, create the following new array.
+["ALBANIA", "BOLIVIA", "CANADA", "DENMARK", "ETHIOPIA", "FINLAND", "GERMANY", "HUNGARY", "IRELAND", "JAPAN", "KENYA"]*/
+
+// const country = [
+//     'Albania',
+//     'Bolivia',
+//     'Canada',
+//     'Denmark',
+//     'Ethiopia',
+//     'Finland',
+//     'Germany',
+//     'Hungary',
+//     'Ireland',
+//     'Japan',
+//     'Kenya'
+//   ]
+
+// let arr = [];
+// let counter = 0;
+// while (counter < country.length) {
+//   arr.push(country[counter].toUpperCase());
+//   counter ++;
+// }
+// console.log(arr);
+
+/*5. Using the above countries array, create an array for countries length'.
+[7, 7, 6, 7, 8, 7, 7, 7, 7, 5, 5]*/
+
+// let arr = [];
+// let counter = 0;
+// while (counter < country.length) {
+//   arr.push(country[counter].length);
+//   counter ++;
+// }
+// console.log(arr);
+
+/*6. Use the countries array to create the following array of arrays:
+  [
+  ['Albania', 'ALB', 7],
+  ['Bolivia', 'BOL', 7],
+  ['Canada', 'CAN', 6],
+  ['Denmark', 'DEN', 7],
+  ['Ethiopia', 'ETH', 8],
+  ['Finland', 'FIN', 7],
+  ['Germany', 'GER', 7],
+  ['Hungary', 'HUN', 7],
+  ['Ireland', 'IRE', 7],
+  ['Iceland', 'ICE', 7],
+  ['Japan', 'JAP', 5],
+  ['Kenya', 'KEN', 5]
+]
+
+7. In above countries array, check if there is a country or countries containing the word 'land'. If there are countries containing 'land', print it as array. If there is no country containing the word 'land', print 'All these countries are without land'.
+  ['Finland','Ireland', 'Iceland']
+
+8. In above countries array, check if there is a country or countries end with a substring 'ia'. If there are countries end with, print it as array. If there is no country containing the word 'ai', print 'These are countries ends without ia'.
+['Albania', 'Bolivia','Ethiopia']
+
+9.Using the above countries array, find the country containing the biggest number of characters.
+Ethiopia
+
+10. Using the above countries array, find the country containing only 5 characters.
+['Japan', 'Kenya']
+
+11. Find the longest word in the webTechs array
+
+12. Use the webTechs array to create the following array of arrays:
+[["HTML", 4], ["CSS", 3],["JavaScript", 10],["React", 5],["Redux", 5],["Node", 4],["MongoDB", 7]]
+
+13.An application created using MongoDB, Express, React and Node is called a MERN stack app. Create the acronym MERN by using the array mernStack
+
+14. Iterate through the array, ["HTML", "CSS", "JS", "React", "Redux", "Node", "Express", "MongoDB"] using a for loop or for of loop and print out the items.
+
+15. This is a fruit array , ['banana', 'orange', 'mango', 'lemon'] reverse the order using loop without using a reverse method. 
+
+16. Print all the elements of array as shown below.
+  const fullStack = [
+    ['HTML', 'CSS', 'JS', 'React'],
+    ['Node', 'Express', 'MongoDB']
+  ]
+
+  HTML
+  CSS
+  JS
+  REACT
+  NODE
+  EXPRESS
+  MONGODB
+*/
+
+
+const country = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ]
+
+let arr = [];
+let counter = 0;
+while (counter < country.length) {
+  arr.push(country[counter].length);
+  counter ++;
+}
+console.log(arr);
