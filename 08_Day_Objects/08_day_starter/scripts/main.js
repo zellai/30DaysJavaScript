996070150575cc32b2ea2712388ac47284cd1286
Ezel Espera <zellai022890@gmail.com>
2024-01-05 10:36:27 +0800

    Day 8: Objects Exercise Level 2 done

--- 08_Day_Objects/08_day_starter/scripts/main.js
+++ 08_Day_Objects/08_day_starter/scripts/main.js
@@ -89,86 +89,106 @@ Set new properties the dog object: breed, getDogInfo.
 
 3. Find people who are MERN stack developer from the users object
 
+// // Function to check if a user is a MERN stack developer
+// function isMERNStackDeveloper(user) {
+//   const mernSkills = ['MongoDB', 'Express', 'React', 'Node'];
+
+//   return mernSkills.every(skill => user.skills.includes(skill));
+// }
+
+// // Find MERN stack developers
+// const mernDevelopers = Object.keys(users).filter(username => isMERNStackDeveloper(users[username]));
+
+// console.log("MERN stack developers:", mernDevelopers);
+
+
 4. Set your name in the users object without modifying the original users object
+
+// const copyUsers = Object.assign({}, users)
+// copyUsers.Ezel = {
+//   email: 'ezel@ezel.com',
+//   skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+//   age: 33,
+//   isLoggedIn: false,
+//   points: 50
+// }
+
+// console.log(copyUsers)
 
 5. Get all keys or properties of users object
 
+// console.log(Object.keys(users));
+
 6. Get all the values of users object
+
+// for(const user in users){
+//   values = users[user];
+//   console.log(values);
+// }
 
 7. Use the countries object to print a country name, capital, populations and languages.*/
 
-const users = {
-  Alex: {
-    email: 'alex@alex.com',
-    skills: ['HTML', 'CSS', 'JavaScript'],
-    age: 20,
-    isLoggedIn: false,
-    points: 30
-  },
-  Asab: {
-    email: 'asab@asab.com',
-    skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
-    age: 25,
-    isLoggedIn: false,
-    points: 50
-  },
-  Brook: {
-    email: 'daniel@daniel.com',
-    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
-    age: 30,
-    isLoggedIn: true,
-    points: 50
-  },
-  Daniel: {
-    email: 'daniel@alex.com',
-    skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
-    age: 20,
-    isLoggedIn: false,
-    points: 40
-  },
-  John: {
-    email: 'john@john.com',
-    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
-    age: 20,
-    isLoggedIn: true,
-    points: 50
-  },
-  Thomas: {
-    email: 'thomas@thomas.com',
-    skills: ['HTML', 'CSS', 'JavaScript', 'React'],
-    age: 20,
-    isLoggedIn: false,
-    points: 40
-  },
-  Paul: {
-    email: 'paul@paul.com',
-    skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
-    age: 20,
-    isLoggedIn: false,
-    points: 40
-  }
-}
+// const users = {
+//   Alex: {
+//     email: 'alex@alex.com',
+//     skills: ['HTML', 'CSS', 'JavaScript'],
+//     age: 20,
+//     isLoggedIn: false,
+//     points: 30
+//   },
+//   Asab: {
+//     email: 'asab@asab.com',
+//     skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+//     age: 25,
+//     isLoggedIn: false,
+//     points: 50
+//   },
+//   Brook: {
+//     email: 'daniel@daniel.com',
+//     skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+//     age: 30,
+//     isLoggedIn: true,
+//     points: 50
+//   },
+//   Daniel: {
+//     email: 'daniel@alex.com',
+//     skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+//     age: 20,
+//     isLoggedIn: false,
+//     points: 40
+//   },
+//   John: {
+//     email: 'john@john.com',
+//     skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+//     age: 20,
+//     isLoggedIn: true,
+//     points: 50
+//   },
+//   Thomas: {
+//     email: 'thomas@thomas.com',
+//     skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+//     age: 20,
+//     isLoggedIn: false,
+//     points: 40
+//   },
+//   Paul: {
+//     email: 'paul@paul.com',
+//     skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+//     age: 20,
+//     isLoggedIn: false,
+//     points: 40
+//   }
+// }
 
-function countUsersWithPointsAbove50(users) {
-    let count = 0;
-    // let personWithMostSkills = null;
-
-    for (const user in users) {
-        const pointsCount = users[user].points;
-
-        if (pointsCount >= 50) {
-            count++;
-        }
-    }
-    return count;
+console.log(countries)
+for(const country in countries){
+  console.log('Country Name: ' + countries[country].name)
+  console.log('Capital: ' + countries[country].capital)
+  console.log('Populations: ' + countries[country].population)
+  console.log('Language: ' + countries[country].languages +"\n\n")
 }
 
 
-const numberPointsAbove50 = countUsersWithPointsAbove50(users);
 
-if (numberPointsAbove50) {
-    console.log(`The number who has 50 and above points is ${numberPointsAbove50}`);
-} else {
-    console.log("No user found.");
-}
+
 
